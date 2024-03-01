df3bb7e64400e71280fe0d2a31e449f2c67cfe1e
coderHARJEET <113464174+coderHARJEET@users.noreply.github.com>
2024-03-01 10:45:21 +0000

    Initial commit

---
+++ spiralmatrix.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> spiralOrder(vector<vector<int>>& matrix) {
+
+        vector<int>ans;
+        int row=matrix.size();
+        int col=matrix[0].size();
+
+        int count=0;
+        int total=row*col;
+
+        //indexes initilaiization
+        int startingRow=0;
+        int startingCol=0;
+        int endingRow=row-1;
+        int endingCol=col-1;
+
+        while(count<total){
+            //print startting row
+            for(int index=startingCol;count<total &&index<=endingCol;index++){
+                ans.push_back(matrix[startingRow][index]);
+                count++;
+            }
+            startingRow++;
+            //print ending col
+            for(int index=startingRow;count<total &&index<=endingRow;index++){
+                ans.push_back(matrix[index][endingCol]);
+                count++;
+            }
+            endingCol--;
+
+            for(int index=endingCol;count<total &&index>=startingCol;index--){
+                ans.push_back(matrix[endingRow][index]);
+                count++;
+            }
+            endingRow--;
+
+            for(int index=endingRow;count<total &&index>=startingRow;index--){
+                ans.push_back(matrix[index][startingCol]);
+                count++;
+            }
+            startingCol++;   
+        }
+        return ans;
+    }
+};
+
+int main() {
+    Solution sol;
+    vector<vector<int>> matrix = {
+        {1, 2, 3},
+        {4, 5, 6},
+        {7, 8, 9}
+    };
+    vector<int> result = sol.spiralOrder(matrix);
+    for (int num : result) {
+        cout << num << " ";
+    }
+    cout << endl;
+    return 0;
+}
